5fce6eeab3cec37bbbfceb19adf3742fb92e6519
XieShuDe <soradd2007@gmail.com>
2012-03-15 16:52:48 +0800

    谢树德：项目代码第一次提交

---
+++ src/cn/com/do1/entity/FootPrint.java
@@ -0,0 +1,118 @@
+package cn.com.do1.entity;
+
+import java.util.Date;
+
+/**
+ * 脚印的实体类
+ * @author Administrator
+ *
+ */
+public class FootPrint {
+
+	private Integer id;
+	private Integer userId;
+	private String latitude;
+	private String longitude;
+	private String altitude;
+	private String time;
+	private String countryName;
+	private String cityName;
+	private String countyName;
+	private String townName;
+	private String villageName;
+	private String mobileType;
+	private String name;
+	private String description;
+	private String photo;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(String altitude) {
+		this.altitude = altitude;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getCountyName() {
+		return countyName;
+	}
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+	public String getTownName() {
+		return townName;
+	}
+	public void setTownName(String townName) {
+		this.townName = townName;
+	}
+	public String getVillageName() {
+		return villageName;
+	}
+	public void setVillageName(String villageName) {
+		this.villageName = villageName;
+	}
+	public String getMobileType() {
+		return mobileType;
+	}
+	public void setMobileType(String mobileType) {
+		this.mobileType = mobileType;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+}
---
+++ src/cn/com/do1/tool/DataBaseHepler.java
@@ -0,0 +1,30 @@
+package cn.com.do1.tool;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * 数据处理工具类
+ * @author Administrator
+ *
+ */
+public class DataBaseHepler extends SQLiteOpenHelper{
+
+	public DataBaseHepler(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+	}
+
+}
